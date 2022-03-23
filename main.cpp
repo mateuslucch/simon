f0cf4208a717393fd5638f1f36253f18624b1d3c
mateuslucch <mateuslucch@gmail.com>
2022-03-23 16:42:58 -0300

    Added sound. Changed second color

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ struct SquareData
     Color secondaryColor;
     float runningTime;
     bool buttonPushed;
+    Sound clickSound;
 };
 
 SquareData ChangeColor(SquareData square, float updateTime, float deltaTime, KeyboardKey keyValue)
 {
+    
     if (IsKeyPressed(keyValue) && square.buttonPushed == false)
     {
+        PlaySound(square.clickSound);
         square.buttonPushed = true;
     }
     if (square.buttonPushed == true)
@@ -40,10 +43,19 @@ int main()
     int windowDimensions[2]{640, 480};
 
     float updateTime{0.2};
-
-    // int gameRounds{};
+   
+    int gameRounds{};
 
     InitWindow(windowDimensions[0], windowDimensions[1], "Simon");
+    InitAudioDevice();
+
+    Sound clickSound1 = LoadSound("Assets/Sound/Randomize1.wav");
+    Sound clickSound2 = LoadSound("Assets/Sound/Randomize2.wav");
+    Sound clickSound3 = LoadSound("Assets/Sound/Randomize3.wav");
+    Sound clickSound4 = LoadSound("Assets/Sound/Randomize4.wav");
+
+    SetSoundVolume(clickSound1, 1);
+    
     int squareSize = 200;
     int gap = 20;
     int xMargin = (windowDimensions[0] - squareSize * 2 - gap) / 2;
@@ -51,35 +63,39 @@ int main()
 
     SquareData yellowSquare;
     yellowSquare.mainColor = YELLOW;
-    yellowSquare.secondaryColor = BLACK;
+    yellowSquare.secondaryColor = GOLD;
     yellowSquare.pos.x = xMargin;
     yellowSquare.pos.y = yMargin + squareSize + gap;
     yellowSquare.size = squareSize;    
     yellowSquare.buttonPushed = false;
+    yellowSquare.clickSound = clickSound1;
 
     SquareData blueSquare;
     blueSquare.mainColor = BLUE;
-    blueSquare.secondaryColor = BLACK;
+    blueSquare.secondaryColor = SKYBLUE;
     blueSquare.pos.x = xMargin + squareSize + gap;
     blueSquare.pos.y = yMargin + squareSize + gap;
     blueSquare.size = squareSize;    
     blueSquare.buttonPushed = false;
+    blueSquare.clickSound = clickSound2;
 
     SquareData greenSquare;
     greenSquare.mainColor = GREEN;
-    greenSquare.secondaryColor = BLACK;
+    greenSquare.secondaryColor = LIME;
     greenSquare.pos.x = xMargin + squareSize + gap;
     greenSquare.pos.y = yMargin;
     greenSquare.size = squareSize;    
     greenSquare.buttonPushed = false;
+    greenSquare.clickSound = clickSound3;
 
     SquareData redSquare;
     redSquare.mainColor = RED;
-    redSquare.secondaryColor = BLACK;
+    redSquare.secondaryColor = MAROON;
     redSquare.pos.x = xMargin;
     redSquare.pos.y = yMargin;
     redSquare.size = squareSize;    
     redSquare.buttonPushed = false;
+    redSquare.clickSound = clickSound4;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -103,5 +119,11 @@ int main()
 
         EndDrawing();
     }
+
+    UnloadSound(clickSound1);
+    UnloadSound(clickSound2);
+    UnloadSound(clickSound3);
+    UnloadSound(clickSound4);
+    CloseAudioDevice();
     CloseWindow();
 }
